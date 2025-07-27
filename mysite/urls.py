f94654beffe5cb6de8b9f046b51b2bc493f048f8
AradRezvaninejad <151847661+AradRezvaninejad@users.noreply.github.com>
2025-07-27 19:25:54 +0330

    added logo and comple the index button

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,17 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
-
+from django.conf import settings
 urlpatterns = []
-
 
 urlpatterns += i18n_patterns(
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
-    path('rosetta/', include('rosetta.urls')), 
-
+    path('rosetta/', include('rosetta.urls')),
 )
